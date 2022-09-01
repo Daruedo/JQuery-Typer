343a2b3cd46f4a911daea2e7cfdc457557b79660
Lucas Eduardo <daruedo@gmail.com>
2022-09-01 18:42:19 -0300

    Adicionando funcionalidade filtro de ID no método GET

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,11 +1,12 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria(){
     $("#spinner").show();
     $("#info").hide();
     $.get("http://localhost:3000/frases", trocaFraseAleatoria)
     .fail(function(){
-        $("#erro").show()
+        $("#erro").toggle()
         setTimeout(function(){
             $("#erro").toggle();
         }, 2500);
@@ -22,4 +23,29 @@ function trocaFraseAleatoria(data){
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo)
+}
+
+function buscaFrase(){
+    $("#spinner").show();
+    $("#info").hide();
+    var fraseId = $("#frase-id").val();
+    var dados = { id: fraseId };
+    $.get("http://localhost:3000/frases",dados,trocaFrase)
+    .fail(function(){
+        $("#erro").toggle()
+        setTimeout(function(){
+            $("#erro").toggle();
+        }, 2500);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+        $("#info").toggle();
+    })
+}
+
+function trocaFrase(data){
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
